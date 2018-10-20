16b3e776b7a9a6a0fc49d998d4bb4b44e0e84003
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-10-19 17:35:47 -0700

    Rendering three lines

--- js/controller.js
+++ js/controller.js
@@ -1,15 +1,45 @@
+const SIZE = 500;
+
 export default class Controller {
 
 	constructor() {
-
+		this.period = 5;
+		this.animAmt = 0;
 	}
 
 	update(dt) {
-		// TODO: Some updating logic
+		this.animAmt += dt / this.period;
+		this.animAmt %= 1;
 	}
 
+	/**
+	 * @param {CanvasRenderingContext2D} context 
+	 */
 	render(context) {
-		// TODO: Some rendering logic
+		const size = 500;
+		const offset = 100 * this.animAmt;
+		const thickness = 50;
+		const numLines = 3;
+
+		const top = offset - thickness / 2;
+		const bottom = offset + thickness / 2;
+
+		context.beginPath();
+		context.fillStyle = 'black';
+
+		for (let i = 0; i < numLines; i ++) {
+			context.rotate(2 * Math.PI / numLines);
+
+			context.moveTo(-size, top);
+			context.lineTo(size, top);
+			context.lineTo(size, bottom);
+			context.lineTo(-size, bottom);
+			context.closePath();
+		}
+
+
+		context.fill('evenodd')
+
 	}
 
 }
