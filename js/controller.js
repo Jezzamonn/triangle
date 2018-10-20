3abcb5519e11d19572b93eaecd0845c2a7a572e2
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-10-19 23:36:55 -0700

    Slide the edges outa there

--- js/controller.js
+++ js/controller.js
@@ -28,11 +28,33 @@ export default class Controller {
 		for (let i = 0; i < numLines; i ++) {
 			const localAnimAmt = (this.animAmt + 0 * i) % 1;
 			const offsetAmt = easeInOut(loop(localAnimAmt), 3);
-			const offset = slurp(thickness / 2, thickness, offsetAmt);
+			const offset = slurp(thickness, 2 * thickness, offsetAmt);
+
 			const top = offset - thickness / 2;
 			const bottom = offset + thickness / 2;
+
+			let slideAmt = 2 * this.animAmt - 1;
+			if (slideAmt < 0) {
+				slideAmt = 0;
+			}
+			slideAmt = easeInOut(slideAmt, 3);
+			const slide = slurp(0, 2 * size, slideAmt)
 	
 			context.rotate(2 * Math.PI / numLines);
+
+			context.moveTo(-size + slide, top);
+			context.lineTo( size + slide, top);
+			context.lineTo( size + slide, bottom);
+			context.lineTo(-size + slide, bottom);
+			context.closePath();
+		}
+
+		const numCenterLines = numLines / 2;
+		for (let i = 0; i < numCenterLines; i ++) {
+			const top = -thickness / 2;
+			const bottom = thickness / 2;
+
+			context.rotate(2 * Math.PI / numCenterLines);
 
 			context.moveTo(-size, top);
 			context.lineTo(size, top);
