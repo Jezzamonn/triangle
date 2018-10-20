bce2119759343e4249a1afbc39399389a38b363b
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2018-10-19 23:19:37 -0700

    Move the lines differentially

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-import { loop, easeInOut } from "./util";
+import { loop, easeInOut, slurp } from "./util";
 
 const SIZE = 500;
 
@@ -19,17 +19,19 @@ export default class Controller {
 	 */
 	render(context) {
 		const size = 500;
-		const offset = 100 * easeInOut(loop(this.animAmt), 3);
 		const thickness = 70;
 		const numLines = 3;
-
-		const top = offset - thickness / 2;
-		const bottom = offset + thickness / 2;
 
 		context.beginPath();
 		context.fillStyle = 'black';
 
 		for (let i = 0; i < numLines; i ++) {
+			const localAnimAmt = (this.animAmt + 0.1 * i) % 1;
+			const offsetAmt = easeInOut(loop(localAnimAmt), 3);
+			const offset = slurp(0, 100, offsetAmt);
+			const top = offset - thickness / 2;
+			const bottom = offset + thickness / 2;
+	
 			context.rotate(2 * Math.PI / numLines);
 
 			context.moveTo(-size, top);
